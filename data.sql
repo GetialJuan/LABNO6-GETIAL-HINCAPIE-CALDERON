48a05c65f1e6928420222d5501b92bbda3cf64b5
GetialJuan <getial.juan@correounivalle.edu.co>
2023-01-13 16:16:44 -0500

    data subcontractor

--- data.sql
+++ data.sql
@@ -1,1 +1,20 @@
 -- (Insertar) Datos en la DB
+
+-- Datos de la tabla subcontractor
+INSERT INTO subcontractor (s_lname,s_fname,salary)
+VALUES
+  ('Colon','Magee',3159666),
+  ('Mayer','Hasad',2974530),
+  ('Mcmillan','Darryl',2971228),
+  ('Whitaker','Raya',3132498),
+  ('Dudley','Germaine',3141975),
+  ('Barron','Zia',2952784),
+  ('Clarke','Jeanette',2859911),
+  ('Bond','Hoyt',3187076),
+  ('Norman','Giacomo',2910430),
+  ('Cantrell','Tanisha',3190371),
+  ('Whitaker','Cecilia',2881445),
+  ('Terrell','Renee',3013843),
+  ('Pena','Unity',2996567),
+  ('Nolan','Fletcher',2963763),
+  ('Sykes','Colt',2877839);
