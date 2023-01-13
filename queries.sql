faba1148927e61e4e13212b7cf2e414f6531f181
GetialJuan <getial.juan@correounivalle.edu.co>
2023-01-13 17:57:21 -0500

    Ejercicio 2 Listo

--- queries.sql
+++ queries.sql
@@ -5,3 +5,11 @@
 
 SELECT (s_lname|| ' ' ||s_fname) AS nombre_completo FROM subcontractor 
     WHERE salary > 3000000 LIMIT 5;
+
+-- Ejercicio No.2 Presente el nombre de los constructores (Builder) y subcontratistas (SubContractor) 
+-- que hayan sido contratados en el 2019
+
+SELECT b_lname AS builder_name, s_lname AS subcontractor_name 
+    FROM (SELECT * FROM hire_info NATURAL JOIN builder) AS builder_name
+        NATURAL JOIN subcontractor
+            WHERE EXTRACT(YEAR FROM hire_date) = '2019';
