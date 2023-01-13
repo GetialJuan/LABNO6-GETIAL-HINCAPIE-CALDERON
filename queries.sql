58248947caeebc919fafe09f705fb5728126df86
GetialJuan <getial.juan@correounivalle.edu.co>
2023-01-13 17:31:31 -0500

    Ejercicio 1 Listo

--- queries.sql
+++ queries.sql
@@ -1,1 +1,7 @@
 -- Consultas a la BD
+
+-- Ejercicio No.1 Presente nombre completo de todos los subcontratistas (SubContractor) que tengan 
+-- un salario mayor a 3000000. Asegúrese de que se obtengan 5 registros.
+
+SELECT (s_lname|| ' ' ||s_fname) AS nombre_completo FROM subcontractor 
+    WHERE salary > 3000000 LIMIT 5;
